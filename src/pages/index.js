0126a8307094611126aa604db0ecbef073801b64
r.ghafouri <reza99gh@gmail.com>
2023-08-10 18:22:45 +0330

    the todo components have been imported on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 // import styles from 'styles/Home.module.css';
-import { TrashIcon, PencilAltIcon, CheckIcon } from '@heroicons/react/solid';
 // import useSWR from 'swr';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import List from 'components/todo/list';
+import Add from 'components/todo/add';
 
 // const fetcher = async () => {
 //   const { data } = await axios.get('/api/todos');
@@ -36,6 +37,17 @@ export default function Home() {
       .catch((error) => console.log(error));
   };
 
+  const addTodo = (e, value) => {
+    e.preventDefault();
+    axios
+      .post(`/api/todos/`, { danzare: value })
+      .then(({ data }) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
   if (loading) return <div>loading...</div>;
 
   return (
@@ -49,35 +61,8 @@ export default function Home() {
       </nav>
       <div className={'container p-2 xl:max-w-screen-xl mx-auto'}>
         <section className={'flex items-center justify-center'}>
-          <div
-            className={
-              'w-full max-w-screen-md bg-gray-100 p-2 md:p-4 rounded-xl'
-            }
-          >
-            {data?.todos?.map((item) => (
-              <div
-                key={item.id}
-                className={
-                  'flex items-center justify-between border border-gray-300 mb-4 p-5 md:p-10 rounded-xl hover:bg-gray-200'
-                }
-              >
-                <span className={'text-4xl text-neutral-500'}>
-                  Task {item.title}
-                </span>
-                <div className={'flex gap-x-3 items-center'}>
-                  <button>
-                    <CheckIcon className={'w-6 h-6 stroke-green-400'} />
-                  </button>
-                  <button onClick={() => deleteTodo(item.id)}>
-                    <TrashIcon className={'w-6 h-6 stroke-red-400'} />
-                  </button>
-                  <button>
-                    <PencilAltIcon className={'w-6 h-6 stroke-blue-400'} />
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
+          <Add submitHandler={addTodo} />
+          <List data={data} onDelete={deleteTodo} />
         </section>
       </div>
     </div>
