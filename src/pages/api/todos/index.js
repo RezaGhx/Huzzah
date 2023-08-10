2b45c57df88b28ba6fdc27f5e92dbedfeff7a5b7
r.ghafouri <reza99gh@gmail.com>
2023-08-10 18:22:18 +0330

    new post api has written

--- src/pages/api/todos/index.js
+++ src/pages/api/todos/index.js
@@ -1,7 +1,14 @@
 import { todos } from 'constant/todos';
 
 export default function handler(req, res) {
-  return res.status(200).json({
-    todos,
-  });
+  if (req?.method === 'POST') {
+    const newTodo = {
+      id: Date.now(),
+      title: req?.body?.value,
+    };
+    todos?.push(newTodo);
+    return res.status(201).json({ message: 'با موفقیت اضافه شد.', todos });
+  } else if (req?.method === 'GET') {
+    return res.status(200).json({ todos });
+  }
 }
