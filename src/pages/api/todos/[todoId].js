fe174af7e5850c04fd86c1d55af6915b934ccd84
r.ghafouri <reza99gh@gmail.com>
2023-08-11 23:54:19 +0330

    mongo delete p1

--- src/pages/api/todos/[todoId].js
+++ src/pages/api/todos/[todoId].js
@@ -1,10 +1,16 @@
-import { todos } from 'constant/todos';
+import dbConnect from 'server/utils/dbConnect';
+import Todo from 'server/models/todo';
 
-export default function handler(req, res) {
+dbConnect();
+
+export default async function handler(req, res) {
+  console.log('سلاااااااااااااااااااااااااااااام', req.query);
   const { todoId } = req?.query;
   if (req?.method === 'DELETE') {
-    const index = todos.findIndex((todo) => todo.id === parseInt(todoId));
-    todos.splice(index, 1);
-    return res.status(200).json({ message: 'Deleted successfully!', todos });
+    await Todo.delete(todoId);
+    return res.status(200).json({ todos });
+    // const index = Todo.findIndex((todo) => todo.id === parseInt(todoId));
+    // todos.splice(index, 1);
+    // return res.status(200).json({ message: 'Deleted successfully!', todos });
   }
 }
