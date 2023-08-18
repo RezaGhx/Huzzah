105d09841989bc2353599f04bdee1ac125aa0ed3
r.ghafouri <reza99gh@gmail.com>
2023-08-18 16:01:22 +0330

    update task feature has been added

--- src/pages/api/todos/[todoId].js
+++ src/pages/api/todos/[todoId].js
@@ -4,10 +4,17 @@ import Todo from 'server/models/todo';
 dbConnect();
 
 export default async function handler(req, res) {
-  const { todoId } = req?.query;
-  if (req?.method === 'DELETE') {
-    await Todo.findByIdAndDelete(todoId);
+  const { method, body, query } = req;
+  if (method === 'DELETE') {
+    await Todo.findByIdAndDelete(query?.todoId);
     const todos = await Todo.find({});
     return res.status(200).json({ message: 'Deleted successfully!', todos });
+  } else if (method === 'PUT') {
+    await Todo.findByIdAndUpdate(
+      query?.todoId,
+      { $set: body?.formData }
+    );
+    const todos = await Todo.find({});
+    return res.status(200).json({ todos });
   }
 }
