0131e33e15bb316c8f77b3229d814a42a29b4935
r.ghafouri <reza99gh@gmail.com>
2023-08-18 18:02:06 +0330

    get by id api has been written

--- src/pages/api/todos/[todoId].js
+++ src/pages/api/todos/[todoId].js
@@ -10,11 +10,16 @@ export default async function handler(req, res) {
     const todos = await Todo.find({});
     return res.status(200).json({ message: 'Deleted successfully!', todos });
   } else if (method === 'PUT') {
-    await Todo.findByIdAndUpdate(
-      query?.todoId,
-      { $set: body?.formData }
-    );
+    await Todo.findByIdAndUpdate(query?.todoId, { $set: body?.formData });
     const todos = await Todo.find({});
     return res.status(200).json({ todos });
+  } else if (method === 'GET') {
+    const todo = await getTodoDetails(query);
+    return res.status(200).json({ message: 'detail loaded', todo });
   }
+}
+
+export async function getTodoDetails(query) {
+  const todo = await Todo.findById(query?.todoId);
+  return todo;
 }
