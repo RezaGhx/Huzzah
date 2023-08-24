e1c36d6f69d68d1ef9c3936ef960fcbaa70c140d
r.ghafouri <reza99gh@gmail.com>
2023-08-24 16:33:32 +0330

    extracting session from our auth datas

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,32 +1,29 @@
 import Link from 'next/link';
-import { signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Header = () => {
+  const { data: session, status } = useSession();
+  console.log(session, status);
+
   return (
     <div className="bg-white w-full shadow-lg">
-      <nav
-        className={'flex justify-between py-4 mb-6 max-w-screen-xl mx-auto'}
-      >
+      <nav className={'flex justify-between py-4 mb-6 max-w-screen-xl mx-auto'}>
         <h1 className={'font-thin'}>
-          <a href='#'>Todo List App</a>
+          <a href="#">Todo List App</a>
         </h1>
-        <ul className='flex items-center gap-x-6'>
+        <ul
+          className={`flex items-center gap-x-6 ${
+            !session && status === 'loading' ? 'opacity-0' : 'opacity-100'
+          }`}
+        >
           <li>
-            <Link href='/'>
-              Home
-            </Link>
-            <Link href='/todos'>
-              Todos
-            </Link>
-            <Link href='/profile'>
-              Profile
-            </Link>
-            <button onClick={() => signIn('github')}>
-              SignIn
-            </button>
-            <button onClick={() => signOut()}>
-              LogOut
-            </button>
+            <Link href="/">Home</Link>
+            <Link href="/todos">Todos</Link>
+            <Link href="/profile">Profile</Link>
+            {!session && status !== 'loading' && (
+              <button onClick={() => signIn('github')}>SignIn</button>
+            )}
+            {session && <button onClick={() => signOut()}>LogOut</button>}
           </li>
         </ul>
       </nav>
